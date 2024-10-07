5a983f70a05a720d7692d80d81eedec5574ec807
Tarek Jaman Labu <tarekjamanlabu515@gmail.com>
2024-10-08 01:01:21 +0600

    Added onsite date to countdown

--- components/sections/countdown.tsx
+++ components/sections/countdown.tsx
@@ -1,17 +1,17 @@
-"use client";
+"use client"
 
-import { fregeist, geist } from "@/app/lib/font";
-import clsx from "clsx";
-import React, { Fragment } from "react";
-import Countdown from "react-countdown";
-import { Label } from "../ui/label";
+import { fregeist, geist } from "@/app/lib/font"
+import clsx from "clsx"
+import React, { Fragment } from "react"
+import Countdown from "react-countdown"
+import { Label } from "../ui/label"
 
 const CountdownSection = () => {
   return (
     <div
       className={clsx(
         fregeist.className,
-        " bg-zinc-900 w-96 space-y-0 px-8 py-6 rounded-3xl mt-44",
+        " bg-zinc-900 w-96 space-y-0 px-8 py-6 rounded-3xl mt-44"
       )}
     >
       <p className="text-2xl py-2 text-zinc-500 text-center">
@@ -20,15 +20,18 @@ const CountdownSection = () => {
       <div
         className={clsx(
           fregeist.className,
-          "text-5xl font-semibold text-white text-center",
+          "text-5xl font-semibold text-white text-center"
         )}
       >
-        <Countdown date={Date.now() + 1000 * 60 * 60 * 24} autoStart />
+        <Countdown
+          date={new Date(Date.parse("19 October, 2024 09:00 AM"))}
+          autoStart
+        />
       </div>
       <div
         className={clsx(
           fregeist.className,
-          "flex gap-10 justify-center items-center mx-auto w-44",
+          "flex gap-10 justify-center items-center mx-auto w-44"
         )}
       >
         <Label className="text-zinc-400 text-base font-medium">Day</Label>
@@ -37,7 +40,7 @@ const CountdownSection = () => {
         <Label className="text-zinc-400 text-base font-medium">Sec</Label>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CountdownSection;
+export default CountdownSection
