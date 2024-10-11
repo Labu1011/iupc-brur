a1468d891bb0cf60222f59eea872554cd91517fa
Fahim Al Islam <fahim6668@gmail.com>
2024-10-11 16:43:16 +0600

    Add: meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,29 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import type {Metadata} from "next"
+import {Inter} from "next/font/google"
 import "./globals.css"
-import { fregeist, geist } from "./lib/font"
-import CustomCursor from "@/components/ui/customCursor"
-import { AppWrapper } from "@/context"
-import { Toaster } from "@/components/ui/toaster"
+import {geist} from "./lib/font"
+import {AppWrapper} from "@/context"
+import {Toaster} from "@/components/ui/toaster"
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({subsets: ["latin"]})
 
 export const metadata: Metadata = {
-  title: "BRUR CSE FEST 2024",
-  description: "",
+	title: "BRUR CSE FEST 2024",
+	description: "",
 }
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
+									   children,
+								   }: Readonly<{
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <link rel="icon" href="/favicon.ico" sizes="any" />
-      <AppWrapper>
-        <body className={geist.className}>{children}</body>
-        <Toaster />
-      </AppWrapper>
-    </html>
-  )
+	return (
+		<html lang="en">
+		<link rel="icon" href="/favicon.ico" sizes="any"/>
+		<AppWrapper>
+			<body className={geist.className}>{children}</body>
+			<Toaster/>
+		</AppWrapper>
+		</html>
+	)
 }
