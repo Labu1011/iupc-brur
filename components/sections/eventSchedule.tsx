ae3d34b4074c49972c255978a22d1afcc97a8394
Fahim Al Islam <fahim6668@gmail.com>
2024-10-17 20:57:02 +0600

    Update: schedule

--- components/sections/eventSchedule.tsx
+++ components/sections/eventSchedule.tsx
@@ -1,90 +1,82 @@
-import { cn } from "@/app/lib/cn"
-import { fregeist, geist } from "@/app/lib/font"
+import {cn} from "@/app/lib/cn"
+import {fregeist, geist} from "@/app/lib/font"
 import React from "react"
 import SectionHeader from "./header"
-import {
-  Table,
-  TableCaption,
-  TableHeader,
-  TableRow,
-  TableHead,
-  TableBody,
-  TableCell,
-} from "../ui/table"
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow,} from "../ui/table"
 
 interface Schedule {
-  event_: string
-  location: string
-  dateTime: string
+	event_: string
+	location: string
+	dateTime: string
 }
 
 const schedule: Schedule[] = [
-  {
-    event_: "Final Register Start",
-    location: "Online",
-    dateTime: "8 Oct, 2024",
-  },
-  {
-    event_: "Final Register End",
-    location: "Online",
-    dateTime: "12 Oct, 2024",
-  },
-  {
-    event_: "Mock Contest",
-    location: "BRUR",
-    dateTime: "18 Oct, 2024, 4:00 PM",
-  },
-  {
-    event_: "Main Contest",
-    location: "BRUR",
-    dateTime: "19 Oct, 2024, 9:00 AM",
-  },
+	{
+		event_: "Final Register Start",
+		location: "Online",
+		dateTime: "8 Oct, 2024",
+	},
+	{
+		event_: "Final Register End",
+		location: "Online",
+		dateTime: "12 Oct, 2024",
+	},
+	{
+		event_: "Mock Contest",
+		location: "BRUR",
+		dateTime: "18 Oct, 2024, 3:00 PM",
+	},
+	{
+		event_: "Main Contest",
+		location: "BRUR",
+		dateTime: "19 Oct, 2024, 12:30 PM",
+	},
 ]
 
 const EventSchedule = () => {
-  return (
-    <div
-      id="events"
-      className={cn(
-        fregeist.className,
-        "relative container space-y-3 bg-black text-white w-full"
-      )}
-    >
-      <div className="w-full h-44"></div>
-      <SectionHeader
-        title={
-          <>
-            Event<span className="text-gray-400/50">.</span>Schedule
-            <span className="text-gray-400/50">()</span>
-          </>
-        }
-        subtitle={<>Check out important dates and times of the RDCPC event.</>}
-      />
-
-      <div className={cn(geist.className, "max-w-[1024px] px-4 mx-auto")}>
-        <Table className="mt-16">
-          <TableHeader className="bg-zinc-900 rounded-xl">
-            <TableRow>
-              <TableHead className="">Event</TableHead>
-              <TableHead>Location</TableHead>
-              <TableHead className="text-right text-right">
-                Date & Time
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {schedule.map((item: Schedule) => (
-              <TableRow key={item.event_}>
-                <TableCell className="font-medium">{item.event_}</TableCell>
-                <TableCell>{item.location}</TableCell>
-                <TableCell className="text-right">{item.dateTime}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
-    </div>
-  )
+	return (
+		<div
+			id="events"
+			className={cn(
+				fregeist.className,
+				"relative container space-y-3 bg-black text-white w-full"
+			)}
+		>
+			<div className="w-full h-44"></div>
+			<SectionHeader
+				title={
+					<>
+						Event<span className="text-gray-400/50">.</span>Schedule
+						<span className="text-gray-400/50">()</span>
+					</>
+				}
+				subtitle={<>Check out important dates and times of the RDCPC event.</>}
+			/>
+			
+			<div className={cn(geist.className, "max-w-[1024px] px-4 mx-auto")}>
+				<Table className="mt-16">
+					<TableHeader className="bg-zinc-900 rounded-xl">
+						<TableRow>
+							<TableHead className="">Event</TableHead>
+							<TableHead>Location</TableHead>
+							<TableHead className="text-right">
+								Date & Time
+							</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{schedule.map((item: Schedule) => (
+							<TableRow key={item.event_}>
+								<TableCell className="font-medium">{item.event_}</TableCell>
+								<TableCell>{item.location}</TableCell>
+								<TableCell className="text-right">{item.dateTime}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</div>
+		</div>
+	)
 }
 
 export default EventSchedule
--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,65 +1,72 @@
 import React from "react"
-
-import Image from "next/image"
-import { cn } from "@/app/lib/cn"
-import { fregeist, geist } from "@/app/lib/font"
-import { TypeAnimation } from "react-type-animation"
-import { ArrowDownIcon, DotIcon } from "@radix-ui/react-icons"
+import {cn} from "@/app/lib/cn"
+import {geist} from "@/app/lib/font"
+import {DotIcon, ClockIcon} from "@radix-ui/react-icons"
 import Link from "next/link"
-import { Button } from "@/components/ui/button"
-import { Toaster } from "@/components/ui/toaster"
-import { useAppContext } from "@/context"
+import {Button} from "@/components/ui/button"
+import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert";
 
 const Hero = () => {
-  return (
-    <div id="/" className="relative bg-[rgba(18, 20, 21, 0)] container w-full">
-      <div className="flex flex-col justfy-center space-y-3 lg:space-y-6 relative z-[1000]">
-        <div className="h-10 lg:h-16"></div>
-        <div className="flex text-center gap-x-2 text-gray-400 items-center mx-auto w-fit bg-light bg-gray-400/10 px-6 py-2 rounded-full border-gray-700">
-          Compete <DotIcon /> Code <DotIcon /> Conquer
-        </div>
-        <p
-          className={cn(
-            geist.className,
-            "text-center leading-[100%] bg-gradient-to-br from-gray-50 to-gray-400 bg-clip-text text-transparent  text-[39px] md:text-[60px] lg:text-[70px]  font-bold"
-          )}
-        >
-          Rangpur Divisional Collegiate <br />
-          Programming Contest
-        </p>
-        <p className="text-lg md:text-2xl text-center text-gray-400 font-regular">
-          Organized by Department of CSE, BRUR
-        </p>
-        <div className="h-0"></div>
-
-        <Link href="/registeredTeam">
-          <Button
-            size={"lg"}
-            className="mx-auto w-fit text-md h-12 px-12 flex group gap-x-2"
-          >
-            View All Teams
-          </Button>
-        </Link>
-      </div>
-
-      {/* <Link */}
-      {/*   href="#services" */}
-      {/*   className="z-10 left-[4%] bottom-[10%] absolute flex items-center justify-center border-white bg-transparent border-4 p-4 shadow-xl rounded-full h-16 w-16" */}
-      {/* > */}
-      {/*   <div className="relative bg-white rounded-full p-2"> */}
-      {/*     <ArrowDownIcon className="h-8 w-8" /> */}
-      {/*     <span className="animate-ping absolute inset-0 inline-flex h-full w-full rounded-full bg-white opacity-75"></span> */}
-      {/*   </div> */}
-      {/* </Link> */}
-      {/* <Image
+	return (
+		<div id="/" className="relative bg-[rgba(18, 20, 21, 0)] container w-full">
+			<div className="flex flex-col justfy-center space-y-3 lg:space-y-6 relative z-[1000]">
+				<div className="h-10 lg:h-16"></div>
+				<div
+					className="flex text-center gap-x-2 text-gray-400 items-center mx-auto w-fit bg-light bg-gray-400/10 px-6 py-2 rounded-full border-gray-700">
+					Compete <DotIcon/> Code <DotIcon/> Conquer
+				</div>
+				<p
+					className={cn(
+						geist.className,
+						"text-center leading-[100%] bg-gradient-to-br from-gray-50 to-gray-400 bg-clip-text text-transparent  text-[39px] md:text-[60px] lg:text-[70px]  font-bold"
+					)}
+				>
+					Rangpur Divisional Collegiate <br/>
+					Programming Contest
+				</p>
+				<p className="text-lg md:text-2xl text-center text-gray-400 font-regular">
+					Organized by Department of CSE, BRUR
+				</p>
+				<div className="h-0"></div>
+				
+				<Link href="/registeredTeam">
+					<Button
+						size={"lg"}
+						className="mx-auto w-fit text-md h-12 px-12 flex group gap-x-2"
+					>
+						View All Teams
+					</Button>
+				</Link>
+			
+			</div>
+			
+			<Alert className="dark my-28 max-w-3xl mx-auto">
+				<ClockIcon className="h-4 w-4 text-yellow-300" />
+				<AlertTitle>RDCPC Schedule has been changed</AlertTitle>
+				<AlertDescription className="opacity-75">
+					Check out the updated schedule below
+				</AlertDescription>
+			</Alert>
+			
+			
+			{/* <Link */}
+			{/*   href="#services" */}
+			{/*   className="z-10 left-[4%] bottom-[10%] absolute flex items-center justify-center border-white bg-transparent border-4 p-4 shadow-xl rounded-full h-16 w-16" */}
+			{/* > */}
+			{/*   <div className="relative bg-white rounded-full p-2"> */}
+			{/*     <ArrowDownIcon className="h-8 w-8" /> */}
+			{/*     <span className="animate-ping absolute inset-0 inline-flex h-full w-full rounded-full bg-white opacity-75"></span> */}
+			{/*   </div> */}
+			{/* </Link> */}
+			{/* <Image
         className="absolute left-1/2 -translate-x-1/2"
         src="/hero.png"
         width={600}
         height={0}
         alt=""
       /> */}
-    </div>
-  )
+		</div>
+	)
 }
 
 export default Hero
